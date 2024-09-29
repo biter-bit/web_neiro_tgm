67bcf93f9a921b0876d906959a52788ea70a0842
Миша Курашев <kurashevmichael@gmail.com>
2024-09-29 22:47:33 +0300

    afdasfdaffsadf

--- main.py
+++ main.py
@@ -29,30 +29,33 @@ def format_request_data(request):
 
 @app.get('/result', response_class=HTMLResponse)
 async def result_confirm(request: Request):
-    body = await request.body()  # Получаем тело запроса
-    headers = request.headers  # Получаем заголовки запроса
-    form_data = await request.form()
-
-    logger.info(f"Request body: {body.decode('utf-8')}")  # Логируем тело
-    logger.info(f"Request headers: {headers}")  # Логируем заголовки
-    invoice = await api_invoice_async.get_invoice(form_data.get("InvId"))
-    if not invoice:
-        logger.error(f"Not invoice ERROR | {invoice}")
-        return "ERROR"
-
-    price = form_data.get("OutSum")
-    inv_id = form_data.get("InvId")
-    email = form_data.get("EMail")
-    signature = form_data.get("SignatureValue")
-
-    if not robokassa_obj.check_signature(inv_id=inv_id, price=price, recv_signature=signature):
-        logger.error(f"Check signature ERROR | {inv_id}")
-        return "Check signature ERROR"
-
-    if email and invoice.profile.email != email.lower():
-        await api_profile_async.update_email(invoice.profiles.id, email.lower())
-    profile = await api_profile_async.update_tariff_of_profile(invoice.profiles.id, 2)
-    return f"OK{inv_id}"
+    formatted_request = format_request_data(request)
+    logger.info({"request": request, "request_data": formatted_request})
+    return "ERROR"
+    # body = await request.body()  # Получаем тело запроса
+    # headers = request.headers  # Получаем заголовки запроса
+    # form_data = await request.form()
+    #
+    # logger.info(f"Request body: {body.decode('utf-8')}")  # Логируем тело
+    # logger.info(f"Request headers: {headers}")  # Логируем заголовки
+    # invoice = await api_invoice_async.get_invoice(form_data.get("InvId"))
+    # if not invoice:
+    #     logger.error(f"Not invoice ERROR | {invoice}")
+    #     return "ERROR"
+    #
+    # price = form_data.get("OutSum")
+    # inv_id = form_data.get("InvId")
+    # email = form_data.get("EMail")
+    # signature = form_data.get("SignatureValue")
+    #
+    # if not robokassa_obj.check_signature(inv_id=inv_id, price=price, recv_signature=signature):
+    #     logger.error(f"Check signature ERROR | {inv_id}")
+    #     return "Check signature ERROR"
+    #
+    # if email and invoice.profile.email != email.lower():
+    #     await api_profile_async.update_email(invoice.profiles.id, email.lower())
+    # profile = await api_profile_async.update_tariff_of_profile(invoice.profiles.id, 2)
+    # return f"OK{inv_id}"
 
 @app.get("/success", response_class=HTMLResponse)
 async def success_payment(request: Request):
